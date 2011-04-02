e5a656ed8114dfe4d761997fe5309c56b55fd756
Jarek Potiuk <jarek.potiuk@polidea.pl>
2011-04-02 14:26:49 +0200

    Updated build scripts and first test runner in place (not yet tests printed - just classes)

---
+++ README.txt
@@ -0,0 +1,2 @@
+This is an instrumentation test runner that is used to generate XML junit-like output of
+android tests. The instrumentation runner extends original android instrumentation runner.
---
+++ src/pl/polidea/instrumentation/PolideaInstrumentationTestRunner.java
@@ -0,0 +1,210 @@
+package pl.polidea.instrumentation;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import junit.framework.AssertionFailedError;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestListener;
+import android.os.Bundle;
+import android.test.AndroidTestRunner;
+import android.test.InstrumentationTestRunner;
+import android.util.Log;
+
+/**
+ * Test runner that should produce Junit-compatible test results. It can be used
+ * to produce output that is parseable by a CI reporting tool which understands
+ * Junit XML format (Jenkins, Hudson, Bamboo ...).
+ * 
+ * @author potiuk
+ * 
+ */
+public class PolideaInstrumentationTestRunner extends InstrumentationTestRunner {
+    private static final String DEFAULT_JUNIT_FILE_NAME = "TEST-all.xml";
+    private String junitOutputFilePath = null;
+    private boolean junitOutputEnabled = false;
+
+    private static final String TAG = PolideaInstrumentationTestRunner.class.getSimpleName();
+
+    /**
+     * Last file where test results are written
+     */
+    private File outputFile;
+    /**
+     * The start time of our current test in System.currentTimeMillis().
+     */
+    private long startTime;
+    /**
+     * The last test class we executed code from.
+     */
+    private Class< ? extends TestCase> lastClass;
+    private PrintWriter outputFileWriter;
+
+    private class JunitTestListener implements TestListener {
+
+        /**
+         * The minimum time we expect a test to take.
+         */
+        private static final int MINIMUM_TIME = 100;
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public void startTest(final Test test) {
+            if (test instanceof TestCase) {
+                if (test.getClass() != lastClass) {
+                    endSingleClass();
+                    lastClass = (Class< ? extends TestCase>) test.getClass();
+                }
+                Thread.currentThread().setContextClassLoader(test.getClass().getClassLoader());
+                startTime = System.currentTimeMillis();
+            }
+        }
+
+        @Override
+        public void endTest(final Test test) {
+            if (test instanceof TestCase) {
+                cleanup((TestCase) test);
+                /*
+                 * Make sure all tests take at least MINIMUM_TIME to complete.
+                 * If they don't, we wait a bit. The Cupcake Binder can't handle
+                 * too many operations in a very short time, which causes
+                 * headache for the CTS.
+                 */
+                final long timeTaken = System.currentTimeMillis() - startTime;
+
+                if (timeTaken < MINIMUM_TIME) {
+                    try {
+                        Thread.sleep(MINIMUM_TIME - timeTaken);
+                    } catch (final InterruptedException ignored) {
+                        // We don't care.
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void addError(final Test test, final Throwable t) {
+            // This space intentionally left blank.
+        }
+
+        @Override
+        public void addFailure(final Test test, final AssertionFailedError t) {
+            // This space intentionally left blank.
+        }
+
+        /**
+         * Nulls all non-static reference fields in the given test class. This
+         * method helps us with those test classes that don't have an explicit
+         * tearDown() method. Normally the garbage collector should take care of
+         * everything, but since JUnit keeps references to all test cases, a
+         * little help might be a good idea.
+         * 
+         * Note! This is copied from InstrumentationCoreTestRunner in android
+         * code
+         */
+        private void cleanup(final TestCase test) {
+            Class< ? > clazz = test.getClass();
+
+            while (clazz != TestCase.class) {
+                final Field[] fields = clazz.getDeclaredFields();
+                for (final Field field : fields) {
+                    final Field f = field;
+                    if (!f.getType().isPrimitive() && !Modifier.isStatic(f.getModifiers())) {
+                        try {
+                            f.setAccessible(true);
+                            f.set(test, null);
+                        } catch (final Exception ignored) {
+                            // Nothing we can do about it.
+                        }
+                    }
+                }
+
+                clazz = clazz.getSuperclass();
+            }
+        }
+
+    }
+
+    private void endSingleClass() {
+        if (lastClass == null) {
+            return;
+        }
+        final Class< ? extends TestCase> clazz = lastClass;
+        if (outputFile != null) {
+            final int errors = 0;
+            final int failures = 0;
+            final String thePackage = clazz.getPackage().getName();
+            final String name = clazz.getSimpleName();
+            final int tests = 10;
+            final String timestamp = "TIMESTAMP";
+            final int time = 10;
+            outputFileWriter.write("<testsuite " + "errors=\"" + errors + "\" " + "failures=\"" + failures + "\" "
+                    + "name=\"" + name + "\" " + "package=\"" + thePackage + "\" " + "tests=\"" + tests + "\" "
+                    + "time=\"" + time + "\" " + "timestamp=\"" + timestamp + "\" " + ">\n");
+            outputFileWriter.write("<properties/>\n");
+            outputFileWriter.write("<system-out>\n");
+            outputFileWriter.write("</system-out>\n");
+            outputFileWriter.write("<system-err>\n");
+            outputFileWriter.write("</system-err>\n");
+            outputFileWriter.write("<testsuite>\n");
+        }
+    }
+
+    private void startFile() {
+        outputFile = new File(getJunitOutputFilePath());
+        try {
+            outputFileWriter = new PrintWriter(outputFile, "UTF-8");
+            outputFileWriter.write("<testsuites>\n");
+        } catch (final IOException e) {
+            Log.e(TAG, "Cannot open file for writing " + e, e);
+            outputFile = null;
+        }
+    }
+
+    private void endFile() {
+        if (outputFile != null) {
+            outputFileWriter.write("</testsuites>\n");
+            outputFileWriter.flush();
+            outputFileWriter.close();
+        }
+    }
+
+    private String getJunitOutputFilePath() {
+        if (junitOutputFilePath == null) {
+            return getTargetContext().getFilesDir().getAbsolutePath() + File.separator + DEFAULT_JUNIT_FILE_NAME;
+        } else {
+            return junitOutputFilePath;
+        }
+    }
+
+    @Override
+    public void onCreate(final Bundle arguments) {
+        if (arguments != null) {
+            junitOutputEnabled = arguments.getBoolean("junitOutput", false);
+            junitOutputFilePath = arguments.getString("junitOutputFile");
+        }
+        super.onCreate(arguments);
+        startFile();
+    }
+
+    @Override
+    public void onDestroy() {
+        endSingleClass();
+        endFile();
+        super.onDestroy();
+    }
+
+    @Override
+    protected AndroidTestRunner getAndroidTestRunner() {
+        final AndroidTestRunner runner = super.getAndroidTestRunner();
+        if (junitOutputEnabled) {
+            runner.addTestListener(new JunitTestListener());
+
+        }
+        return runner;
+    }
+}
